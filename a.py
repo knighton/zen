4ed4562a5d797812008e90ea10d758137e8c4c7e
James Knighton <iamknighton@gmail.com>
2017-08-30 21:57:51 -0700

    Use methods.

--- a.py
+++ a.py
@@ -1,23 +1,41 @@
+import numpy as np
 import torch
 from torch.autograd import Variable
 
-dtype = torch.cuda.FloatTensor
+
+def variable(arr, tensor_type):
+    tensor = torch.from_numpy(arr).type(tensor_type)
+    return Variable(tensor, requires_grad=True)
+
+
+def constant(arr, tensor_type):
+    tensor = torch.from_numpy(arr).type(tensor_type)
+    return Variable(tensor, requires_grad=False)
+
+
+def init(*shape):
+    return np.random.normal(0., 1., shape)
+
+
+def mean_squared_error(true, pred):
+    return (true - pred).pow(2).sum()
+
+
+tt = torch.cuda.FloatTensor
 
 batch_size, in_dim, hidden_dim, num_classes = 64, 1000, 100, 10
 
-x = Variable(torch.randn(batch_size, in_dim).type(dtype), requires_grad=False)
-y = Variable(torch.randn(batch_size, num_classes).type(dtype),
-             requires_grad=False)
+x = constant(init(batch_size, in_dim), tt)
+y_true = constant(init(batch_size, num_classes), tt)
 
-w1 = Variable(torch.randn(in_dim, hidden_dim).type(dtype), requires_grad=True)
-w2 = Variable(torch.randn(hidden_dim, num_classes).type(dtype),
-              requires_grad=True)
+w1 = variable(init(in_dim, hidden_dim), tt)
+w2 = variable(init(hidden_dim, num_classes), tt)
 
 learning_rate = 1e-6
 for t in range(500):
     y_pred = x.mm(w1).clamp(min=0).mm(w2)
 
-    loss = (y_pred - y).pow(2).sum()
+    loss = mean_squared_error(y_true, y_pred)
     print(t, loss.data[0])
 
     loss.backward()
