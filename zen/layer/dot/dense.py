cbc948e31d4ff9c1e50258a6c0f3fd9912ad4744
James Knighton <iamknighton@gmail.com>
2017-08-30 23:10:53 -0700

    DenseSpec: allow out_dim to be None (get shape from input).

--- zen/layer/dot/dense.py
+++ zen/layer/dot/dense.py
@@ -13,12 +13,16 @@ class DenseLayer(Layer):
 
 
 class DenseSpec(Spec):
-    def __init__(self, out_dim):
+    def __init__(self, out_dim=None):
         super().__init__()
         self.out_dim = out_dim
 
     def build(self, in_shape=None, in_dtype=None):
         in_dim, = in_shape
-        out_shape = self.out_dim,
-        layer = DenseLayer(init(in_dim, self.out_dim), init(self.out_dim))
+        if self.out_dim is None:
+            out_dim = in_dim
+        else:
+            out_dim = self.out_dim
+        out_shape = out_dim,
+        layer = DenseLayer(init(in_dim, out_dim), init(out_dim))
         return layer, out_shape, in_dtype
