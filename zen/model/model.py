5e70ade282110abd636481b7c6dcf1b0adadd09d
James Knighton <iamknighton@gmail.com>
2017-08-31 03:14:13 -0700

    Pull out per-batch code.

--- zen/model/model.py
+++ zen/model/model.py
@@ -12,6 +12,28 @@ class Model(object):
     def forward(self, x, is_training):
         raise NotImplementedError
 
+    def train_on_batch(self, x, y_true, opt):
+        x = constant(x)
+        y_true = constant(y_true)
+        y_pred = self.forward(x, True)
+        loss = categorical_cross_entropy(y_true, y_pred)
+        loss_value = loss.data.cpu().numpy()[0]
+        loss.backward()
+        opt.step()
+        acc = categorical_accuracy(y_true, y_pred)
+        acc_value = acc.data.cpu().numpy()[0]
+        return loss_value, acc_value
+
+    def evaluate_on_batch(self, x, y_true):
+        x = constant(x)
+        y_true = constant(y_true)
+        y_pred = self.forward(x, False)
+        loss = categorical_cross_entropy(y_true, y_pred)
+        loss_value = loss.data.cpu().numpy()[0]
+        acc = categorical_accuracy(y_true, y_pred)
+        acc_value = acc.data.cpu().numpy()[0]
+        return loss_value, acc_value
+
     def train_classifier_on_epoch(self, data, opt, batch_size, epoch):
         (x_train, y_train), (x_val, y_val) = data
         num_train_batches = len(x_train) // batch_size
@@ -21,27 +43,17 @@ class Model(object):
         for j in range(num_train_batches):
             a = j * batch_size
             z = (j + 1) * batch_size
-            x = constant(x_train[a:z])
-            y_true = constant(y_train[a:z])
-            y_pred = self.forward(x, True)
-            loss = categorical_cross_entropy(y_true, y_pred)
-            train_losses.append(loss.data.cpu().numpy()[0])
-            loss.backward()
-            acc = categorical_accuracy(y_true, y_pred)
-            train_accs.append(acc.data.cpu().numpy()[0])
-            opt.step()
+            loss, acc = self.train_on_batch(x_train[a:z], y_train[a:z], opt)
+            train_losses.append(loss)
+            train_accs.append(acc)
         val_losses = []
         val_accs = []
         for j in range(num_val_batches):
             a = j * batch_size
             z = (j + 1) * batch_size
-            x = constant(x_train[a:z])
-            y_true = constant(y_train[a:z])
-            y_pred = self.forward(x, False)
-            loss = categorical_cross_entropy(y_true, y_pred)
-            val_losses.append(loss.data.cpu().numpy()[0])
-            acc = categorical_accuracy(y_true, y_pred)
-            val_accs.append(acc.data.cpu().numpy()[0])
+            loss, acc = self.evaluate_on_batch(x_val[a:z], y_val[a:z])
+            val_losses.append(loss)
+            val_accs.append(acc)
         train_loss = np.array(train_losses).mean()
         train_acc = np.array(train_accs).mean()
         val_loss = np.array(val_losses).mean()
