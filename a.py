3feffb744dec262a3809367b061dd64cac5e232c
James Knighton <iamknighton@gmail.com>
2017-08-31 02:39:47 -0700

    Also note accuracy for sanity.

--- a.py
+++ a.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 
 from zen import constant
 from zen.dataset.mnist import load_mnist
-from zen.functional import categorical_cross_entropy
+from zen.functional import categorical_accuracy, categorical_cross_entropy
 from zen.layer import *
 from zen.optim import MVO
 from zen.transform.one_hot import one_hot
@@ -27,15 +27,16 @@ spec = SequenceSpec(
 
 model, out_shape, out_dtype = spec.build()
 
-opt = MVO(1e-1)
+opt = MVO(0.2)
 opt.set_params(model.get_params())
 
 batch_size = 64
-num_epochs = 10
+num_epochs = 1000
 num_train_batches = len(x_train) // batch_size
 num_val_batches = len(x_val) // batch_size
 for i in range(num_epochs):
     train_losses = []
+    train_accs = []
     for j in range(num_train_batches):
         a = j * batch_size
         z = (j + 1) * batch_size
@@ -45,8 +46,11 @@ for i in range(num_epochs):
         loss = categorical_cross_entropy(y_true, y_pred)
         train_losses.append(loss.data.cpu().numpy()[0])
         loss.backward()
+        acc = categorical_accuracy(y_true, y_pred)
+        train_accs.append(acc.data.cpu().numpy()[0])
         opt.step()
     val_losses = []
+    val_accs = []
     for j in range(num_val_batches):
         a = j * batch_size
         z = (j + 1) * batch_size
@@ -55,6 +59,11 @@ for i in range(num_epochs):
         y_pred = model.forward(x, False)
         loss = categorical_cross_entropy(y_true, y_pred)
         val_losses.append(loss.data.cpu().numpy()[0])
+        acc = categorical_accuracy(y_true, y_pred)
+        val_accs.append(acc.data.cpu().numpy()[0])
     train_loss = np.array(train_losses).mean()
+    train_acc = np.array(train_accs).mean()
     val_loss = np.array(val_losses).mean()
-    print('epoch %4d train %.4f val %.4f' % (i, train_loss, val_loss))
+    val_acc = np.array(val_accs).mean()
+    print('epoch %4d train %.4f (%.2f%%) val %.4f (%.2f%%)' %
+          (i, train_loss, train_acc, val_loss, val_acc))
--- zen/functional/__init__.py
+++ zen/functional/__init__.py
@@ -1,3 +1,4 @@
+import torch
 from torch.nn import functional as F
 
 
@@ -40,3 +41,17 @@ def categorical_cross_entropy(true, pred):
 
 def mean_squared_error(true, pred):
     return (true - pred).pow(2).mean()
+
+
+def binary_accuracy(true, pred):
+    ret = true == pred.round()
+    ret = ret.type(torch.cuda.FloatTensor)
+    return 100. * ret.mean()
+
+
+def categorical_accuracy(true, pred):
+    true = true.max(1)[1]
+    pred = pred.max(1)[1]
+    ret = true == pred
+    ret = ret.type(torch.cuda.FloatTensor)
+    return 100. * ret.mean()
