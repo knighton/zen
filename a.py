f76c9077763145132629697c6faae8debfa4d653
James Knighton <iamknighton@gmail.com>
2017-08-30 21:46:00 -0700

    Make variable names sane.

--- a.py
+++ a.py
@@ -3,13 +3,15 @@ from torch.autograd import Variable
 
 dtype = torch.cuda.FloatTensor
 
-N, D_in, H, D_out = 64, 1000, 100, 10
+batch_size, in_dim, hidden_dim, num_classes = 64, 1000, 100, 10
 
-x = Variable(torch.randn(N, D_in).type(dtype), requires_grad=False)
-y = Variable(torch.randn(N, D_out).type(dtype), requires_grad=False)
+x = Variable(torch.randn(batch_size, in_dim).type(dtype), requires_grad=False)
+y = Variable(torch.randn(batch_size, num_classes).type(dtype),
+             requires_grad=False)
 
-w1 = Variable(torch.randn(D_in, H).type(dtype), requires_grad=True)
-w2 = Variable(torch.randn(H, D_out).type(dtype), requires_grad=True)
+w1 = Variable(torch.randn(in_dim, hidden_dim).type(dtype), requires_grad=True)
+w2 = Variable(torch.randn(hidden_dim, num_classes).type(dtype),
+              requires_grad=True)
 
 learning_rate = 1e-6
 for t in range(500):
