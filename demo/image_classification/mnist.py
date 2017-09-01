4c81c16af6028b651a79207c5f2849d1ebb01ca0
James Knighton <iamknighton@gmail.com>
2017-09-01 13:29:07 -0700

    run().

--- demo/image_classification/mnist.py
+++ demo/image_classification/mnist.py
@@ -5,25 +5,36 @@ from zen.layer import *
 from zen.transform.one_hot import one_hot
 
 
-(x_train, y_train), (x_val, y_val) = load_mnist()
-image_shape = x_train.shape[1:]
-num_classes = y_train.max() + 1
-y_train = one_hot(y_train, num_classes)
-y_val = one_hot(y_val, num_classes)
-data = (x_train, y_train), (x_val, y_val)
+def parse_args():
+    ap = ArgumentParser()
+    ap.add_argument('--verbose', type=int, default=2)
+    ap.add_argument('--stop', type=int, default=1000)
+    return ap.parse_args()
 
-spec = SequenceSpec(
-    Input(image_shape),
-    Flatten,
-    Dense(256),
-    ReLU,
-    Dense(64),
-    ReLU,
-    Dense(num_classes),
-    Softmax
-)
 
-model, out_shape, out_dtype = spec.build()
+def run(args):
+    (x_train, y_train), (x_val, y_val) = load_mnist(args.verbose)
+    image_shape = x_train.shape[1:]
+    num_classes = y_train.max() + 1
+    y_train = one_hot(y_train, num_classes)
+    y_val = one_hot(y_val, num_classes)
+    data = (x_train, y_train), (x_val, y_val)
 
-num_epochs = 1000
-model.train_classifier(data, stop=num_epochs)
+    spec = SequenceSpec(
+        Input(image_shape),
+        Flatten,
+        Dense(256),
+        ReLU,
+        Dense(64),
+        ReLU,
+        Dense(num_classes),
+        Softmax
+    )
+
+    model, out_shape, out_dtype = spec.build()
+
+    model.train_classifier(data, stop=args.stop)
+
+
+if __name__ == '__main__':
+    run(parse_args())
