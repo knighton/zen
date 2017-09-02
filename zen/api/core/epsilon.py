ab79b7d3653d49462368609a8e82f07f889fb757
James Knighton <iamknighton@gmail.com>
2017-09-02 03:03:35 -0700

    set_epsilon().

--- zen/api/core/epsilon.py
+++ zen/api/core/epsilon.py
@@ -3,3 +3,11 @@ _EPSILON = 1e-6
 
 def epsilon():
     return _EPSILON
+
+
+def set_epsilon(x):
+    assert isinstance(x, float), \
+        'Epsilon must be a float (got %s): %s.' % (type(x), x)
+    assert 0. < x, 'Epsilon must be positive (got %s).' % x
+    global _EPSILON
+    _EPSILON = x
