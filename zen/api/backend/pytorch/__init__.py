4db89645bc3e729d74ffd9c671f5964f6469784d
James Knighton <iamknighton@gmail.com>
2017-09-02 13:10:11 -0700

    Split pooling (api/ and api/backend/pytorch/).

--- zen/api/backend/pytorch/__init__.py
+++ zen/api/backend/pytorch/__init__.py
@@ -2,3 +2,4 @@ from .core import *  # noqa
 from .curve import *  # noqa
 from .dot import *  # noqa
 from .fuzz import *  # noqa
+from .shape import *  # noqa
--- zen/api/shape/pool.py
+++ zen/api/shape/pool.py
@@ -1,127 +1,70 @@
 from math import floor
-from torch.nn import functional as F
 
-from ..core.data import get_ndim
-from ..core.util import to_shape
-from .pad import pad3d
-
-from ..backend.pytorch.core.util import to_one  # TODO: fix
+from .. import backend as Z
+from .. import core as C
 
 
-def avg_pool1d(x, window, padding, stride):
-    """
-    x        variable (NCW)
-    window   dim or shape (W)
-    padding  dim or shape (W)
-    stride   dim or shape (W)
-    """
-    window = to_one(window)
-    padding = to_one(padding)
-    stride = to_one(stride)
-    return F.avg_pool1d(x, window, stride, padding)
+"""
+Average pooling.
+
+    x, window, padding, stride -> y
+
+Input:
+                           1D   2D    3D
+                           --   --    --
+    x        variable      NCW  NCHW  NCDHW
+    window   dim or shape  W    HW    DHW
+    padding  dim or shape  W    HW    DHW
+    stride   dim or shape  W    HW    DHW
+
+Output:
+    x        variable      NCW  NCHW  NCDHW
+"""
+avg_pool = Z.avg_pool
+avg_pool1d = Z.avg_pool1d
+avg_pool2d = Z.avg_pool2d
+avg_pool3d = Z.avg_pool3d
 
 
-def avg_pool2d(x, window, padding, stride):
-    """
-    x        variable (NCHW)
-    window   dim or shape (HW)
-    padding  dim or shape (HW)
-    stride   dim or shape (HW)
-    """
-    return F.avg_pool2d(x, window, stride, padding)
+"""
+Max pooling.
 
+    x, window, padding, stride -> y
 
-def avg_pool3d(x, window, padding, stride):
-    """
-    x        variable (NCDHW)
-    window   dim or shape (DHW)
-    padding  dim or shape (DHW)
-    stride   dim or shape (DHW)
-    """
-    x = pad3d(x, padding)
-    return F.avg_pool3d(x, window, stride)
+Input:
+                           1D   2D    3D
+                           --   --    --
+    x        variable      NCW  NCHW  NCDHW
+    window   dim or shape  W    HW    DHW
+    padding  dim or shape  W    HW    DHW
+    stride   dim or shape  W    HW    DHW
 
-
-_DIM2AVG_POOL = {
-    1: avg_pool1d,
-    2: avg_pool2d,
-    3: avg_pool3d,
-}
-
-
-def avg_pool(x, window, padding, stride):
-    """
-    x        variable (NC...)
-    window   dim or shape
-    padding  dim or shape
-    stride   dim or shape
-    """
-    dim = get_ndim(x) - 2
-    return _DIM2AVG_POOL[dim](x, window, padding, stride)
-
-
-def max_pool1d(x, window, padding, stride):
-    """
-    x        variable (NCW)
-    window   dim or shape (W)
-    padding  dim or shape (W)
-    stride   dim or shape (W)
-    """
-    window = to_one(window)
-    padding = to_one(padding)
-    stride = to_one(stride)
-    return F.max_pool1d(x, window, stride, padding)
-
-
-def max_pool2d(x, window, padding, stride):
-    """
-    x        variable (NCHW)
-    window   dim or shape (HW)
-    padding  dim or shape (HW)
-    stride   dim or shape (HW)
-    """
-    return F.max_pool2d(x, window, stride, padding)
-
-
-def max_pool3d(x, window, padding, stride):
-    """
-    x        variable (NCDHW)
-    window   dim or shape (DHW)
-    padding  dim or shape (DHW)
-    stride   dim or shape (DHW)
-    """
-    return F.max_pool3d(x, window, stride, padding)
-
-
-_DIM2MAX_POOL = {
-    1: max_pool1d,
-    2: max_pool2d,
-    3: max_pool3d,
-}
-
-
-def max_pool(x, window, padding, stride):
-    """
-    x        variable (NC...)
-    window   dim or shape
-    padding  dim or shape
-    stride   dim or shape
-    """
-    dim = get_ndim(x) - 2
-    return _DIM2MAX_POOL[dim](x, window, padding, stride)
+Output:
+    y        variable      NCW  NCHW  NCDHW
+"""
+max_pool = Z.max_pool
+max_pool1d = Z.max_pool1d
+max_pool2d = Z.max_pool2d
+max_pool3d = Z.max_pool3d
 
 
 def pool_out_shape(in_shape, window, padding, stride):
     """
-    in_shape  shape (of x without channel dim)
-    window    dim or shape
-    padding   dim or shape
-    stride    dim or shape
+    Compute pooling output shape.
+
+    Input:
+        in_shape   shape (of x without channel dim)
+        window     dim or shape
+        padding    dim or shape
+        stride     dim or shape
+
+    Output:
+        out_shape  shape (of y without channel dim)
     """
     dim = len(in_shape)
-    window = to_shape(window, dim)
-    padding = to_shape(padding, dim)
-    stride = to_shape(stride, dim)
+    window = C.to_shape(window, dim)
+    padding = C.to_shape(padding, dim)
+    stride = C.to_shape(stride, dim)
     out_shape = [None] * len(in_shape)
     for i in range(len(in_shape)):
         value = (in_shape[i] + 2 * padding[i] - window[i]) / stride[i]
