dbc5c8635ba7613e97d697996e85670b761505c8
James Knighton <iamknighton@gmail.com>
2017-09-06 05:17:37 -0700

    Sentiment140 dataset.

---
+++ demo/sentiment_analysis/sentiment140.py
@@ -0,0 +1,66 @@
+from argparse import ArgumentParser
+import string
+import sys
+
+from zen.dataset.sentiment140 import load_sentiment140
+from zen.layer import *
+from zen.transform import *
+
+
+def parse_args():
+    ap = ArgumentParser()
+    ap.add_argument('--verbose', type=int, default=2)
+    ap.add_argument('--val_frac', type=float, default=0.2)
+    ap.add_argument('--model', type=str, default='cnn')
+    ap.add_argument('--opt', type=str, default='adam')
+    ap.add_argument('--stop', type=int, default=1000)
+    return ap.parse_args()
+
+
+def cnn(review_len, vocab_size):
+    conv = lambda n: SequenceSpec(Conv(n), BatchNorm, ReLU, MaxPool)
+    cnn = []
+    for n in [64] * 4:
+        cnn.append(conv(n))
+    cnn = SequenceSpec(*cnn)
+    in_shape = review_len,
+    spec = SequenceSpec(
+        Input(in_shape, dtype='int64'), Embed(vocab_size, 64), cnn, Flatten,
+        Dense(1), Sigmoid)
+    model, out_shape, out_dtype = spec.build()
+    return model
+
+
+def eru(review_len, vocab_size):
+    in_shape = review_len,
+    spec = SequenceSpec(
+        Input(in_shape, dtype='int64'), Embed(vocab_size, 64), ERU(64),
+        Dropout(0.1), ERU(64, ret='last'), Dropout(0.1), Dense(1), Sigmoid)
+    model, out_shape, out_dtype = spec.build()
+    return model
+
+
+def transform(data, text_pipe, label_pipe):
+    train_texts = text_pipe.fit_transform(data[0][0])
+    train_labels = label_pipe.fit_transform(data[0][1])
+    val_texts = text_pipe.transform(data[1][0])
+    val_labels = label_pipe.transform(data[1][1])
+    return (train_texts, train_labels), (val_texts, val_labels)
+
+
+def run(args):
+    module = sys.modules[__name__]
+    build = getattr(module, args.model)
+    data = load_sentiment140(args.val_frac, args.verbose)
+    text_pipe = Pipe(Lower, Filter(string.ascii_lowercase + ' '), Split,
+                     Length(32), Dict, NDArray('int64'))
+    label_pipe = Pipe(NDArray('float32'))
+    data = transform(data, text_pipe, label_pipe)
+    review_len = data[0][0].shape[1]
+    vocab_size = int(data[0][0].max() + 1)
+    model = build(review_len, vocab_size)
+    model.train_classifier(data, opt=args.opt, stop=args.stop)
+
+
+if __name__ == '__main__':
+    run(parse_args())
---
+++ zen/dataset/sentiment140.py
@@ -0,0 +1,99 @@
+import numpy as np
+import os
+from random import shuffle
+from tqdm import tqdm
+from zipfile import ZipFile
+
+from .util import download, get_dataset_dir
+
+
+_DATASET_NAME = 'sentiment140'
+_URL = 'http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip'
+_ZIP_PATH = 'training.1600000.processed.noemoticon.csv'
+_ENCODINGS = ['utf-8', 'latin-1']
+_PROCESSED_SUBDIR = 'processed'
+_POS_BASENAME = 'pos.txt'
+_NEG_BASENAME = 'neg.txt'
+
+
+def _write_tweets(tweets, filename, verbose):
+    with open(filename, 'wb') as out:
+        if verbose == 2:
+            tweets = tqdm(tweets)
+        for tweet in tweets:
+            assert '\n' not in tweet
+            line = (tweet + '\n').encode('utf-8')
+            out.write(line)
+
+
+def _process(local, processed_dir, verbose):
+    os.mkdir(processed_dir)
+    zip_file = ZipFile(local)
+    lines = zip_file.open(_ZIP_PATH).readlines()
+    if verbose == 2:
+        lines = tqdm(lines)
+    pos = []
+    neg = []
+    for line in lines:
+        for encoding in _ENCODINGS:
+            try:
+                line = line.decode(encoding)
+            except:
+                pass
+        ss = line[1:-1].split('","')
+        assert len(ss) == 6
+        sent = int(ss[0])
+        if sent == 4:
+            tweets = pos
+        elif sent == 0:
+            tweets = neg
+        else:
+            assert False
+        tweet = ss[5]
+        tweets.append(tweet)
+    filename = os.path.join(processed_dir, _POS_BASENAME)
+    _write_tweets(pos, filename, verbose)
+    filename = os.path.join(processed_dir, _NEG_BASENAME)
+    _write_tweets(neg, filename, verbose)
+
+
+def _read_tweets(filename, verbose):
+    tweets = []
+    lines = open(filename)
+    if verbose == 2:
+        lines = tqdm(lines)
+    tweets = []
+    for line in lines:
+        tweet = line[:-1]
+        tweets.append(tweet)
+    return tweets
+
+
+def _load(processed_dir, val_frac, verbose):
+    filename = os.path.join(processed_dir, _POS_BASENAME)
+    pos = _read_tweets(filename, verbose)
+    filename = os.path.join(processed_dir, _NEG_BASENAME)
+    neg = _read_tweets(filename, verbose)
+    samples = list(map(lambda tweet: (tweet, 1), pos)) + \
+              list(map(lambda tweet: (tweet, 0), neg))
+    shuffle(samples)
+    x, y = list(zip(*samples))
+    split = int(len(x) * val_frac)
+    x_train = x[split:]
+    x_val = x[:split]
+    y_train = y[split:]
+    y_val = y[:split]
+    y_train = np.array(y_train)
+    y_val = np.array(y_val)
+    return (x_train, y_train), (x_val, y_val)
+
+
+def load_sentiment140(val_frac=0.2, verbose=2):
+    dataset_dir = get_dataset_dir(_DATASET_NAME)
+    processed_dir = os.path.join(dataset_dir, _PROCESSED_SUBDIR)
+    if not os.path.exists(processed_dir):
+        zip_basename = os.path.basename(_URL)
+        local = os.path.join(dataset_dir, zip_basename)
+        download(_URL, local, verbose)
+        _process(local, processed_dir, verbose)
+    return _load(processed_dir, val_frac, verbose)
