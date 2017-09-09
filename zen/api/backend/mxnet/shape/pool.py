40f98492cb20151e6f2b8b25c0a8adf4f99bb13a
James Knighton <iamknighton@gmail.com>
2017-09-09 16:59:00 -0700

    Fixes (mxnet).

--- zen/api/backend/mxnet/shape/pool.py
+++ zen/api/backend/mxnet/shape/pool.py
@@ -19,10 +19,10 @@ avg_pool3d = avg_pool
 
 
 def max_pool(x, window, padding, stride):
-    ndim = C.get_ndim(x)
+    ndim = C.get_ndim(x) - 2
     window = to_shape(window, ndim)
     padding = to_shape(padding, ndim)
-    stride = to_shape(padding, ndim)
+    stride = to_shape(stride, ndim)
     return mx.nd.Pooling(data=x, kernel=window, pad=padding, stride=stride,
                          pool_type='max')
 
