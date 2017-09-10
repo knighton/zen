54e99ebc461357d8d286b2571397995307b576ad
James Knighton <iamknighton@gmail.com>
2017-09-10 01:29:04 -0700

    Quora duplicate questions dataset.

---
+++ demo/text_clf/duplicate_questions.py
@@ -0,0 +1,4 @@
+from zen.dataset.quora_question_pairs import load_quora_question_pairs
+
+
+load_quora_question_pairs()
---
+++ zen/dataset/quora_question_pairs.py
@@ -0,0 +1,38 @@
+import os
+
+from .util import download, get_dataset_dir
+
+
+_DATASET_NAME = 'quora_question_pairs'
+_URL = 'http://qim.ec.quoracdn.net/quora_duplicate_questions.tsv'
+
+
+def _load(filename, verbose):
+    question_1s = []
+    question_2s = []
+    is_dupes = []
+    lines = open(filename).read().strip().split('\n')[1:]
+    for i, line in enumerate(lines):
+        try:
+            id_ = int(line.split()[0])
+        except:
+            lines[i - 1] += lines[i]
+            lines[i] = None
+    lines = list(filter(bool, lines))
+    for line in lines:
+        ss = line.split('\t')
+        q1 = ss[3]
+        question_1s.append(q1)
+        q2 = ss[4]
+        question_2s.append(q2)
+        is_dupe = int(ss[5])
+        assert is_dupe in {0, 1}
+        is_dupes.append(is_dupe)
+    return question_1s, question_2s, np.array(is_dupes, dtype='int64')
+
+
+def load_quora_question_pairs(verbose=2):
+    dataset_dir = get_dataset_dir(_DATASET_NAME)
+    local = os.path.join(dataset_dir, os.path.basename(_URL))
+    download(_URL, local, verbose)
+    return _load(local, verbose)
