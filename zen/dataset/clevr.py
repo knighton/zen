18a33924a1786c35492d99e6d82e0ca75a6dd8f2
James Knighton <iamknighton@gmail.com>
2017-09-10 07:10:17 -0700

    Refactor CLEVR (generalize processed image shape).

--- zen/dataset/clevr.py
+++ zen/dataset/clevr.py
@@ -1,4 +1,4 @@
-import io
+from io import BytesIO
 import json
 import numpy as np
 import os
@@ -24,61 +24,57 @@ _COGENT_SAME_SPLITS = ['trainA', 'valA', 'testA']
 _COGENT_DIFF_SPLITS = ['trainA', 'valB', 'testB']
 _IMAGE_RE = re.compile('.+/images/.+.png')
 _QUESTIONS_RE = re.compile('.+/questions/.+.json')
-
-
-_SIZES = {
+_IMAGE_SHAPES = [
     (60, 40),
     (120, 80),
     (240, 160),
-}
+]
 
 
-def _normalize_size(x):
-    if x is None:
-        x = 60, 40
-    assert x in _SIZES, \
-        'Size %s is not valid.  Possible values: %s.' % (size, _SIZES)
-    return x
+def _normalize_image_shape(shape, possible_image_shapes):
+    if shape is None:
+        shape = 60, 40
+    assert shape in possible_image_shapes, \
+        'Image shape %s is not available.  Possible values: %s.' % \
+            (shape, possible_image_shapes)
+    return shape
 
 
-def _extract_image(zip_file, path):
+def _get_processed_images_filename(processed_dir, split, shape):
+    basename = 'images_%dx%d_%s.bin' % (shape[0], shape[1], split))
+    return os.path.join(processed_dir, basename)
+
+
+def _process_image(zip_file, path, to_shapes, files):
     data = zip_file.open(path).read()
-    image = Image.open(io.BytesIO(data))
+    image = Image.open(BytesIO(data))
     image = image.convert('RGB')
-    assert image.size == (480, 320)
-    _240 = image.resize((240, 160))
-    _240 = np.array(_240)
-    _240 = np.rollaxis(_240, 2)
-    _120 = image.resize((120, 80))
-    _120 = np.array(_120)
-    _120 = np.rollaxis(_120, 2)
-    _60 = image.resize((60, 40))
-    _60 = np.array(_60)
-    _60 = np.rollaxis(_60, 2)
-    return _240.tobytes(), _120.tobytes(), _60.tobytes()
+    assert image.shape == (480, 320)
+    for to_shape, file_ in zip(to_shapes, files):
+        x = image.resize(to_shape)
+        x = np.array(x)
+        x = np.rollaxis(x, 2)
+        file_.write(x.tobytes())
 
 
-def _extract_images(zip_file, zip_paths, split, processed_dir, verbose):
+def _process_images(zip_file, zip_paths, processed_dir, split, image_shapes,
+                    verbose):
     paths = list(filter(_IMAGE_RE.match, zip_paths))
     paths = list(filter(lambda s: split in s, paths))
     paths.sort()
-    out_240 = os.path.join(processed_dir, 'images_240x160_%s.bin' % split)
-    out_240 = open(out_240, 'wb')
-    out_120 = os.path.join(processed_dir, 'images_120x80_%s.bin' % split)
-    out_120 = open(out_120, 'wb')
-    out_60 = os.path.join(processed_dir, 'images_60x40_%s.bin' % split)
-    out_60 = open(out_60, 'wb')
-    gen = paths
+    files = []
+    for shape in image_shapes:
+        filename = _get_processed_images_filename(processed_dir, split, shape)
+        files.append(open(filename, 'wb'))
     if verbose == 2:
-        gen = tqdm(gen, leave=False)
-    for path in gen:
-        _240, _120, _60 = _extract_image(zip_file, path)
-        out_240.write(_240)
-        out_120.write(_120)
-        out_60.write(_60)
+        paths = tqdm(paths, leave=False)
+    for path in paths:
+        _process_image(zip_file, path, image_shapes, files)
+    for file_ in files:
+        file_.close()
 
 
-def _extract_questions(zip_file, zip_paths, split, processed_dir, verbose):
+def _process_questions(zip_file, zip_paths, processed_dir, split, verbose):
     paths = list(filter(_QUESTIONS_RE.match, zip_paths))
     paths = list(filter(lambda s: split in s, paths))
     assert len(paths) == 1
@@ -103,18 +99,20 @@ def _extract_questions(zip_file, zip_paths, split, processed_dir, verbose):
         out.write(line.encode('utf-8'))
 
 
-def _extract_main_split(zip_file, zip_paths, split, processed_dir, verbose):
+def _process_split(zip_file, zip_paths, processed_dir, split, image_shapes,
+                   verbose):
     if verbose:
         print('Extracting %s split...' % split)
         t0 = time()
-    _extract_images(zip_file, zip_paths, split, processed_dir, verbose)
-    _extract_questions(zip_file, zip_paths, split, processed_dir, verbose)
+    _process_images(zip_file, zip_paths, processed_dir, split, image_shapes,
+                    verbose)
+    _process_questions(zip_file, zip_paths, processed_dir, split, verbose)
     if verbose:
         t = time() - t0
         print('...took %.3f sec.' % t)
 
 
-def _extract(zip_filename, processed_dir, splits, verbose):
+def _process(zip_filename, processed_dir, splits, image_shapes, verbose):
     os.mkdir(processed_dir)
     if verbose:
         print('Loading archive at %s' % zip_filename)
@@ -125,17 +123,19 @@ def _extract(zip_filename, processed_dir, splits, verbose):
         t = time() - t0
         print('...took %.3f sec.' % t)
     for split in splits:
-        _extract_main_split(zip_file, zip_paths, split, processed_dir, verbose)
+        _process_split(zip_file, zip_paths, processed_dir, split, image_shapes,
+                       verbose)
 
 
-def _process(dataset_name, processed_subdir, url, all_splits, verbose):
+def _ready(dataset_name, processed_subdir, url, all_splits, image_shapes,
+           verbose):
     dataset_dir = get_dataset_dir(dataset_name)
     processed_dir = os.path.join(dataset_dir, processed_subdir)
     if not os.path.exists(processed_dir):
         filename = os.path.join(dataset_dir, os.path.basename(url))
         if not os.path.exists(filename):
             download(url, filename, verbose)
-        _extract(filename, processed_dir, all_splits, verbose)
+        _process(filename, processed_dir, all_splits, image_shapes, verbose)
     return processed_dir
 
 
@@ -158,16 +158,15 @@ class RamClevrDataset(Dataset):
 
 
 def _load_split(question_pipe, answer_pipe, processed_dir, split,
-                is_train_split, size, storage, verbose):
+                is_train_split, image_shape, storage, verbose):
     assert storage == 'ram'  # XXX
-    assert len(size) == 2
-    filename = os.path.join(
-        processed_dir, 'images_%dx%d_%s.bin' % (size[0], size[1], split))
+    assert len(image_shape) == 2
+    filename = _get_processed_images_filename(processed_dir, split, image_shape)
     print('Reading %s...' % filename)
     data = open(filename, 'rb').read()
     images = np.fromstring(data, dtype='uint8')
     images = images.astype('float32')
-    images = images.reshape(-1, 3, size[0], size[1])
+    images = images.reshape(-1, 3, image_shape[0], image_shape[1])
     filename = os.path.join(processed_dir, 'questions_%s.txt' % split)
     answers = []
     image_ids = []
@@ -190,41 +189,41 @@ def _load_split(question_pipe, answer_pipe, processed_dir, split,
     return RamClevrDataset(images, image_ids, questions, answers)
 
 
-def _load(question_pipe, answer_pipe, processed_dir, splits, size, storage,
-          verbose):
+def _load(question_pipe, answer_pipe, processed_dir, splits, image_shape,
+          storage, verbose):
     assert len(splits) == 3
     train = _load_split(question_pipe, answer_pipe, processed_dir, splits[0],
-                        True, size, storage, verbose)
+                        True, image_shape, storage, verbose)
     val = _load_split(question_pipe, answer_pipe, processed_dir, splits[1],
-                      False, size, storage, verbose)
+                      False, image_shape, storage, verbose)
     return TrainingData(train, val)
 
 
-def load_clevr_main(question_pipe, answer_pipe, storage='ram', size=None,
+def load_clevr_main(question_pipe, answer_pipe, storage='ram', image_shape=None,
                     verbose=2):
     assert storage in {'ram', 'disk'}
-    size = _normalize_size(size)
-    processed_dir = _process(_DATASET_NAME, _MAIN_PROCESSED_SUBDIR, _MAIN_URL,
-                             _MAIN_SPLITS, verbose)
-    return _load(question_pipe, answer_pipe, processed_dir, _MAIN_SPLITS, size,
-                 storage, verbose)
+    image_shape = _normalize_image_shape(image_shape, _IMAGE_SHAPES)
+    processed_dir = _ready(_DATASET_NAME, _MAIN_PROCESSED_SUBDIR, _MAIN_URL,
+                           _MAIN_SPLITS, _IMAGE_SHAPES, verbose)
+    return _load(question_pipe, answer_pipe, processed_dir, _MAIN_SPLITS,
+                 image_shape, storage, verbose)
 
 
-def load_clevr_cogent_same(question_pipe, answer_pipe, storage='ram', size=None,
-                           verbose=2):
+def load_clevr_cogent_same(question_pipe, answer_pipe, storage='ram',
+                           image_shape=None, verbose=2):
     assert storage in {'ram', 'disk'}
-    size = _normalize_size(size)
-    processed_dir = _process(_DATASET_NAME, _COGENT_PROCESSED_SUBDIR,
-                             _COGENT_URL, _COGENT_ALL_SPLITS, verbose)
+    image_shape = _normalize_image_shape(image_shape, _IMAGE_SHAPES)
+    processed_dir = _ready(_DATASET_NAME, _COGENT_PROCESSED_SUBDIR, _COGENT_URL,
+                           _COGENT_ALL_SPLITS, _IMAGE_SHAPES, verbose)
     return _load(question_pipe, answer_pipe, processed_dir, _COGENT_SAME_SPLITS,
-                 size, storage, verbose)
+                 image_shape, storage, verbose)
 
 
-def load_clevr_cogent_diff(question_pipe, answer_pipe, storage='ram', size=None,
-                           verbose=2):
+def load_clevr_cogent_diff(question_pipe, answer_pipe, storage='ram',
+                           image_shape=None, verbose=2):
     assert storage in {'ram', 'disk'}
-    size = _normalize_size(size)
-    processed_dir = _process(_DATASET_NAME, _COGENT_PROCESSED_SUBDIR,
-                             _COGENT_URL, _COGENT_ALL_SPLITS, verbose)
+    image_shape = _normalize_image_shape(image_shape, _IMAGE_SHAPES)
+    processed_dir = _ready(_DATASET_NAME, _COGENT_PROCESSED_SUBDIR, _COGENT_URL,
+                           _COGENT_ALL_SPLITS, _IMAGE_SHAPES, verbose)
     return _load(question_pipe, answer_pipe, processed_dir, _COGENT_DIFF_SPLITS,
-                 size, storage, verbose)
+                 image_shape, storage, verbose)
