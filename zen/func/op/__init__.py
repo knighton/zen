f8083dbf70819139ef590bd286e6bffcf84793f3
James Knighton <iamknighton@gmail.com>
2017-09-18 12:33:12 -0700

    Rename: fuzz -> fuzzy.

--- zen/func/op/__init__.py
+++ zen/func/op/__init__.py
@@ -1,6 +1,6 @@
 from .curve import *  # noqa
 from .dot import *  # noqa
-from .fuzz import *  # noqa
+from .fuzzy import *  # noqa
 from .geometry import *  # noqa
 from .merge import *  # noqa
 from .project import *  # noqa
--- zen/layer/__init__.py
+++ zen/layer/__init__.py
@@ -1,7 +1,7 @@
 from .arch import *  # noqa
 from .curve import *  # noqa
 from .dot import *  # noqa
-from .fuzz import *  # noqa
+from .fuzzy import *  # noqa
 from .geometry import *  # noqa
 from .merge import *  # noqa
 from .project import *  # noqa
