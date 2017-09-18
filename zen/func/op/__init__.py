bed1d0dce81c9aaec29423cbb5289a02319a51b5
James Knighton <iamknighton@gmail.com>
2017-09-18 12:30:08 -0700

    proj -> project.

--- zen/func/op/__init__.py
+++ zen/func/op/__init__.py
@@ -3,5 +3,5 @@ from .dot import *  # noqa
 from .fuzz import *  # noqa
 from .geometry import *  # noqa
 from .merge import *  # noqa
-from .proj import *  # noqa
+from .project import *  # noqa
 from .relational import *  # noqa
